e5412d2fa17a9de31855304b2d0eaaad55774b26
khaleddaidar012 <khaleddaidar2@gmail.com>
2025-04-22 09:12:52 +0200

    Update bookRoutes.js
    add edit api

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -79,7 +79,33 @@ router.delete('/:id' , async (req,res) =>{
         response.status(500).send({ message: error.message });
         }
 
-})
+});
+router.put('/:id', async (request, response) => {
+    try {
+    if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+    ) {
+        return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+        });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+        return response.status(404).json({ message: 'Book not found' });
+    }
+
+    return response.status(200).send({ message: 'Book updated successfully' });
+    } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+    }
+});
 
 
 export default router;
